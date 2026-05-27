ddc7e89e2e668a0025896ebd5e1d46d612b75403
bananestar <s.vanderlinden13@gmail.com>
2026-05-27 20:13:14 +0200

    feat(bot): add debug logging for sleep conditions in setupAutoSleep function

--- utils/botLife.js
+++ utils/botLife.js
@@ -163,6 +163,7 @@ function setupAutoSleep(bot, isUserWhitelistedMC) {
 	function canSleep() {
 		const isNight = bot.time.timeOfDay >= 12542 && bot.time.timeOfDay <= 23460;
 		const isThunder = bot.thunderState > 0;
+		Logger.debug(`canSleep: time=${bot.time.timeOfDay} isNight=${isNight} thunderState=${bot.thunderState} isRaining=${bot.isRaining} isThunder=${isThunder}`);
 		return isNight || isThunder;
 	}
 
